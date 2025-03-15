8b72537ddf6a02fbdf5c95e3b0b787cddf70e3d5
Ben Knowles <ben@ionophore.com>
2025-03-15 11:25:54 -0700

    Clean up and convert to C++

--- APUE/fork12.cpp
+++ APUE/fork12.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <format>
 #include <iterator>
-#include <vector>
+#include <string>
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
 	std::ostream_iterator<char> oit(std::cout);
-	std::format_to(oit,"%");;
 
-	std::vector<char> input(1024,'\0');
-	while (std::fgets(input.data(), input.size(), stdin)) {
-		std::size_t input_len = strlen(input.data());
-		if (input[input_len-1] == '\n') {
-			input[input_len-1] = '\0';
+	std::string input;
+	input.reserve(1024);
+	while (true) {
+		std::format_to(oit,"%");;
+		std::getline(std::cin, input);
+		if (!std::cin) {
+			break;
 		}
 		
 		pid_t pid = fork();
@@ -24,8 +23,8 @@ int main() {
 			std::format_to(oit,"Fork error :(");
 		} else if (pid == 0) {
 			// Child
-			execlp(input.data(), input.data(), nullptr);
-			std::format_to(oit, "couldn't execute: {}", input.data());
+			execlp(input.c_str(), input.c_str(), nullptr);
+			std::format_to(oit, "couldn't execute: {}\n", input.c_str());
 			std::exit(127);
 		}
 
@@ -35,7 +34,6 @@ int main() {
 		if (pid < 0) {
 			std::format_to(oit, "waitpid error");
 		}
-		std::format_to(oit, "%");
 	}
 
 	return 0;
--- APUE/readme.md
+++ APUE/readme.md
@@ -1,6 +1,5 @@
 ## Example from page 12 (Figure 1.7) of APUE
-Undisciplined mishmash of C and C++<br>
-Mixing iostreams and cstdio is likely going to cause problems (FIX)<br>
+Mostly C++<br>
 <br>
 execlp in the child process replaces that process with whatever program the user is trying to execute.<br>
 <br>
