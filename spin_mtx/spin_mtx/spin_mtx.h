235bae76fd36af1781910f970c7476ae60ab8cdd
Ben Knowles <ben@ionophore.com>
2025-04-06 11:32:50 -0700

    simple spin mtx

---
+++ spin_mtx/spin_mtx/spin_mtx.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <atomic>
+
+class spin_mtx {
+private:
+	std::atomic<bool> m_locked {false};
+public:
+	// Blocks via a spin lock
+	void lock();
+
+	void unlock();
+
+	// True if the lock was successfully taken, ie, the caller now owns the locked state and is responsible
+	// for calling unlock().  False if somebody else owned the lock at the time of the call (the caller does
+	// _not_ own the locked state).
+	bool try_lock();
+};
+
+void verify(bool b);
