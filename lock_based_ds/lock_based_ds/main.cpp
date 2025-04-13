ffe2d66a35da4bca4c1dd3c980e88e93bf732934
Ben Knowles <ben@ionophore.com>
2025-04-13 06:19:40 -0700

    lock-based sll

--- lock_based_ds/lock_based_ds/main.cpp
+++ lock_based_ds/lock_based_ds/main.cpp
@@ -2,18 +2,47 @@
 #include "lock_based_queue.h"
 #include "sll_q.h"
 #include <iostream>
+#include <thread>
+
 
 int main(int argc, char* argv[]) {
 	std::cout << "Hello CMake." << std::endl;
 
-	sll_queue<int> q1;
-	q1.push(1);
-	q1.push(2);
-	q1.push(3);
+	tssll_queue<int> q1;
 
-	auto val = q1.try_pop();
-	val = q1.try_pop();
-	val = q1.try_pop();
+	auto push_n = [&q1](int n) {
+		for (int i=0; i<n; ++i) {
+			q1.push(i);
+		}
+	};
+
+	std::int64_t sum {0};
+
+	auto pop_n = [&q1,&sum](int n) {
+		int npop {0};
+		while (npop < n) {
+			auto curr = q1.try_pop();
+			if (curr) {
+				++npop;
+				sum += *curr;
+			}
+		}
+	};
+
+	int n = 4;
+	std::thread t1(pop_n,2*n);
+	std::thread t2(push_n,n);
+	std::thread t3(push_n,n);
+	/*std::thread t4(push_n,n);
+	std::thread t5(push_n,n);*/
+
+	t1.join();
+	t2.join();
+	t3.join();
+	/*t4.join();
+	t5.join();*/
+
+	std::cout << sum << std::endl;
 
 	return 0;
 }
--- lock_based_ds/lock_based_ds/sll_q.h
+++ lock_based_ds/lock_based_ds/sll_q.h
@@ -1,5 +1,5 @@
 #include <memory>
-
+#include <mutex>
 
 // Singly-linked-list-based queue
 // Williams p. 183-184
@@ -41,5 +41,55 @@ public:
 		return nullptr;
 	}
 };
+
+
+
+// Multi-threaded version
+// You'd think it would be possible to push and pop at the same time given something like this
+template<typename T>
+class tssll_queue {
+	struct node {
+		T val_ {};
+		std::unique_ptr<node> next_ {};
+	};
+	std::unique_ptr<node> head_ {};
+	node* tail_ {};
+
+	std::mutex mtx_head_;
+	std::mutex mtx_tail_;
+
+public:
+	tssll_queue()=default;
+	tssll_queue(const tssll_queue&)=delete;
+	tssll_queue& operator=(const tssll_queue&)=delete;
+
+	void push(T val) {
+		std::lock_guard lkt(mtx_tail_);
+		if (tail_) {  // q !empty
+			tail_->next_ = std::make_unique<node>(std::move(val),nullptr);
+			tail_ = tail_->next_.get();
+		} else {  // q empty
+			std::lock_guard lkh(mtx_head_);
+			head_ = std::make_unique<node>(val,nullptr);
+			tail_ = head_.get();
+		}
+	}
+
+	std::unique_ptr<T> try_pop() {
+		std::lock_guard lkh(mtx_head_);
+		if (head_) {
+			std::unique_ptr<T> result = std::make_unique<T>(std::move(head_->val_));
+			mtx_tail_.lock();
+			if (tail_==head_.get()) {
+				tail_ = nullptr;
+			}
+			mtx_tail_.unlock();
+			head_ = std::move(head_->next_);
+			return result;
+		}
+		return nullptr;
+	}
+};
+
 
 
