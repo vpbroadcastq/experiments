6a709ee8b508515850553793fa78f4a5987178ed
Ben Knowles <ben@ionophore.com>
2026-07-05 07:47:31 -0700

    Why do the two methods differ???

--- mt_dir_walk/cs/Program.cs
+++ mt_dir_walk/cs/Program.cs
@@ -39,12 +39,14 @@ class Program
         //
         // Directory.EnumerateFileSystemEntries
         //
+        HashSet<string> hs1 = new HashSet<string>();
         EnumerationOptions opts = new EnumerationOptions();
         opts.RecurseSubdirectories = true;
         IEnumerable<string> it = Directory.EnumerateFileSystemEntries(rootPath, "*", opts);
         foreach (string curr in it)
         {
             Console.WriteLine(curr);
+            hs1.Add(curr);
         }
 
         Console.WriteLine("--------------------------------------------------------------------------------------------");
@@ -52,6 +54,7 @@ class Program
         //
         // FsIterator
         //
+        HashSet<string> hs2 = new HashSet<string>();
         FsIterator? fsIt = FsIterator.Create(rootPath);
         if (fsIt == null)
         {
@@ -65,8 +68,27 @@ class Program
                 break;
             }
             Console.WriteLine(curr);
+            hs2.Add(curr);
             fsIt.Next();
         }
+
+
+        Console.WriteLine("--------------------------------------------------------------------------------------------");
+        bool eq = hs1==hs2;
+        if (eq)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not equal");
+            var hs3 = hs2.Except(hs1);
+            foreach(string curr in hs3)
+            {
+                Console.WriteLine(curr);
+            }
+        }
+
     }
 }
 
